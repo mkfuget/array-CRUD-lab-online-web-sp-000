f97d0a449f7e8183dfceaee887b6fa039c8527ad
Michael Fuget <michaelfuget@gmail.com>
2020-12-01 07:04:11 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,8 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  out = ["apple", "banana", "apricot". "pear"]
-  return out
+  return ["apple", "banana", "apricot", "pear"]
 end
 
 def add_element_to_end_of_array(array, element)

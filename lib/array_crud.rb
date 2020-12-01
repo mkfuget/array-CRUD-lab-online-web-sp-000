475bc3e9dd26ef2220c3217ec9c363e2f297c8e3
Michael Fuget <michaelfuget@gmail.com>
2020-12-01 07:03:26 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,25 +1,25 @@
 def create_an_empty_array
-  return array.new
+  return []
 end
 
 def create_an_array
-  
+  return ["apple", "banana", "apricot". "pear"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  return array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  return array.shift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  return array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  return array.unshift
 end
 
 def retrieve_element_from_index(array, index_number)
